9c8c463d35107a3b665f01b69089cb82552113b9
dbormotov <dbormotov@direct-credit.ru>
2022-11-15 22:21:48 +0300

    add yoyo migrations service in project

---
+++ migrations/20221115_01_BKRjS-create-messages-table.py
@@ -0,0 +1,30 @@
+"""
+Create messages table
+"""
+
+from yoyo import step
+
+__depends__ = {}
+
+
+def apply_step(conn):
+    cursor = conn.cursor()
+    cursor.execute(
+        "CREATE TABLE messages("
+        "id INT NOT NULL PRIMARY KEY,"
+        "route VARCHAR(55) NOT NULL,"
+        "message TEXT NOT NULL"
+        ");"
+    )
+
+
+def rollback_step(conn):
+    cursor = conn.cursor()
+    cursor.execute(
+        "DROP TABLE IF EXISTS messages;"
+    )
+
+
+steps = [
+    step(apply_step, rollback_step)
+]
